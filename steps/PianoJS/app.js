3ffebe8de84b6d9badfbc78340187fd23a6bd685
SajadFahimian <s.fahimian0040@gmail.com>
2022-08-07 19:37:37 +0430

    add new logic for play audio file

--- steps/PianoJS/app.js
+++ steps/PianoJS/app.js
@@ -4,19 +4,18 @@ btn.classList.add("inBlock");
 let spans = document.querySelectorAll("span");
 
 let keyRange = ["a", "w", "s", "e", "d", "f", "t", "g", "y", "h", "u", "j", "k", "o", "l", "p", ";"];
-let allAudio = document.getElementsByTagName("audio");
 
 let audioObject = {};
 
-for (let index = 0; index < allAudio.length; index++) {
-    audioObject[keyRange[index]] = allAudio[index];
+for (let index = 0; index < keyRange.length; index++) {
+    audioObject[keyRange[index]] = new Audio(`./audio/${index}.wav`);
 }
 
 for (let span of Array.from(spans)) {
     span.classList.add("none");
 }
 keyRange.push(":");
-async function holdShift(event) {
+function holdShift(event) {
     if (event.key == "Shift") {
         for (let span of Array.from(spans)) {
             span.classList.remove("none");
@@ -28,10 +27,12 @@ async function holdShift(event) {
     }
     if (keyRange.indexOf(event.key.toLowerCase()) != -1) {
         if (event.key == ":") {
-        await audioObject[";"].play();
+        audioObject[";"].load();
+        audioObject[";"].play();
 
         } else {
-        await audioObject[event.key.toLowerCase()].play();
+            audioObject[event.key.toLowerCase()].load();
+            audioObject[event.key.toLowerCase()].play();
         }
 
     }
